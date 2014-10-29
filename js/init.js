3eca51eb4a9b9f4f0109c80079d2fd1c0cafe429
Oscar Vasquez <ozkar2641@gmail.com>
2014-10-28 22:58:21 -0400

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+						   
+	$("#countdown").countdown({
+				date: "30 october 2014 12:00:00",
+				format: "on"
+			},
+			
+			function() {
+				// callback function
+			});
+
+    $(".tweet").tweet({
+        username: "flashuser",
+        count: 3,
+        loading_text: "loading tweets..."
+    });
+
+});	
